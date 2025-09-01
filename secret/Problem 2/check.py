7f5c32e022e0a4aabfc2b7692ba01e73a70c6371
SURYA K <83123427+sharpsk@users.noreply.github.com>
2025-09-01 17:33:34 +0530

    Delete secret/Problem 2 directory

--- secret/Problem 2/check.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("real_power_trace.csv")
-print(df.head())       # show first 5 rows
-print(df.columns)      # show all column names
-print(df.shape)        # show (rows, columns)
